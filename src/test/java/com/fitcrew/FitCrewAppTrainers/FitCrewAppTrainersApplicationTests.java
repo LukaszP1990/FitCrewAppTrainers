c3f7f43042411b451e4f57e45cfb194e2674fc45
Łukasz <lukaszptaszek@interia.com>
2020-03-28 14:36:09 +0100

    migration from java 8 to java 11

--- src/test/java/com/fitcrew/FitCrewAppTrainers/FitCrewAppTrainersApplicationTests.java
+++ src/test/java/com/fitcrew/FitCrewAppTrainers/FitCrewAppTrainersApplicationTests.java
@@ -1,15 +1,13 @@
 package com.fitcrew.FitCrewAppTrainers;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = FitCrewAppTrainersApplication.class)
-public class FitCrewAppTrainersApplicationTests {
+class FitCrewAppTrainersApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
